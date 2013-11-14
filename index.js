847ac6c4059d4ee464220498c533c9a591286ca2
jamesjwood <jamesjwood@msn.com>
2013-11-14 09:18:59 +0000

    edit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var grunt = require('grunt');
-module.exports =  function(){
-  var deps = ['*.js','src/**/*.js', 'test/**/*.js'];
-  var pkg= grunt.file.readJSON('package.json');
-  for(var name in pkg.dependencies)
-  {
-    deps.push('../' + name + '/package.json');
-    deps.push('node_modules/' + name + '/package.json');
-  }
-  return deps;
+var path = require('path');
+
+module.exports =  function(pkg){
+	var deps = ['*.js','src/**/*.js', 'test/**/*.js'];
+	for(var name in pkg.dependencies)
+	{
+  		deps.push('../' + name + '/package.json');
+  		deps.push('node_modules/' + name + '/package.json');
+  	}
+  	return deps;
 }
