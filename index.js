3e2d75777cac11e964783aa5de1c0145bfd64120
jamesjwood <jamesjwood@msn.com>
2013-11-22 08:58:43 +0000

    exclude othe rdeps

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+var watchable={
+	utils: true,
+	jsonCrypto: true,
+	analytics: true,
+	pouchTools: true,
+	artwork: true,
+	core: true,
+	objectTypes: true,
+	data: true,
+	object: true,
+	userService: true,
+	shareService: true,
+	pouchdb: true,
+	webApp: true
+};
+
 module.exports =  function(pkg){
 	var deps = [];
 	for(var name in pkg.dependencies)
 	{
+		if(watchable[name])
+		{
   		deps.push('../' + name + '/package.json');
-  		deps.push('node_modules/' + name + '/package.json');
+  		deps.push('node_modules/' + name + '/package.json');			
+		}
   }
   return deps;
 }
